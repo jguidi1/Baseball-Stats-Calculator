6b0faef3b7566f4f449b278bdb5e334056aafb04
Joseph Guidi <jguidi1@users.noreply.github.com>
2025-11-02 14:26:20 -0500

    Cleaned up with equations and .f rounding - complete

--- StatsCalc.py
+++ StatsCalc.py
@@ -9,7 +9,13 @@ user_BB = int(input("Enter # of walks: "))
 user_hbp = int(input("Enter # of hit by pitch: "))
 user_sf = int(input("Enter # of sacrifice flies: "))
 
-print("Batting Average (BA): "+(round((user_AB/user_H)/10,3)).__str__()) #limited to 3 decimal places
-print("On Base Percentage (OBP): "+(round((user_H+user_BB+user_hbp)/(user_AB+user_BB+user_hbp+user_sf),3)).__str__())
-print("Slugging Percentage (SLG): "+(((user_H-user_doubles-user_triples-user_HR)+(2*user_doubles)+(3*user_triples)+(4*user_HR))/user_AB).__str__())
-print("On Base Plus Slugging (OPS): "+((((user_H+user_BB+user_hbp)/(user_AB+user_BB+user_hbp+user_sf))+(((user_H-user_doubles-user_triples-user_HR)+(2*user_doubles)+(3*user_triples)+(4*user_HR))/user_AB))).__str__())
+BA = user_H/user_AB
+OBP = ((user_H+user_BB+user_hbp)/(user_AB+user_BB+user_hbp+user_sf))
+SLG = (((user_H-user_doubles-user_triples-user_HR)+(2*user_doubles)+(3*user_triples)+(4*user_HR))/user_AB)
+OPS = ((((user_H+user_BB+user_hbp)/(user_AB+user_BB+user_hbp+user_sf))+(((user_H-user_doubles-user_triples-user_HR)+(2*user_doubles)+(3*user_triples)+(4*user_HR))/user_AB)))
+
+print("--- Calculated Stats ---")
+print(f"Batting Average (BA): {BA:.3f}") #limited to 3 decimal places
+print(f"On Base Percentage (OBP): {OBP:.3f}")
+print(f"Slugging Percentage (SLG): {SLG:.3f}")
+print(f"On Base Plus Slugging (OPS): {OPS:.3f}")
